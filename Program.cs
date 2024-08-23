657927bae343a93e90c2231e717b7348dee134ae
Guicollar <guicollar05@gmail.com>
2024-08-23 19:57:15 -0300

    criação dos metodos NOVO e LISTAR PRODUTOS E REMOVER PRODUTOS

--- Program.cs
+++ Program.cs
@@ -12,8 +12,7 @@ class Program
         Menu();
         
     }
-static List<Pedras> pedras = new List<Pedras>();
-static int contador = 1;
+
     static void Menu(){
         Console.WriteLine("Bem vindo ao menu \n");
         Console.WriteLine("1. Novo");
@@ -27,15 +26,17 @@ static int contador = 1;
         string? escolha = Console.ReadLine();
         
             switch (escolha)
-            {
+            { 
                 case "1":
-                    Console.WriteLine("opção 1");
+                    Opcao1();
+
                     break;
                 case "2":
-                    Console.WriteLine("opção 2");
+                    Opcao2();
+
                     break;
                 case "3":
-                    Console.WriteLine("opção 3");
+                    Opcao3();
                     break;
                 case "4":
                     Console.WriteLine("opção 4");
@@ -49,6 +50,7 @@ static int contador = 1;
             }   
             Menu();
     }
+static Estoque estoque = new Estoque();
     static void Opcao1(){
         Console.WriteLine(" digite o nome do material");
         string nome = Console.ReadLine();
@@ -60,10 +62,26 @@ static int contador = 1;
         string fornecedor = Console.ReadLine();
         Console.WriteLine("digite a medida");
         string medida = Console.ReadLine();
+
+        estoque.Novo(nome, valor, tipo, fornecedor, medida);
         
-        
+    
     }
+static void Opcao2(){
+    estoque.ListarProdutos();
 }
+
+static void Opcao3(){
+    Console.WriteLine(" digite o numero do produto em sua lista ");
+    string numero = Console.ReadLine();
+    if (int.TryParse(numero, out int numerador))
+    {
+        estoque.RemoverProdutos(numerador);
+    }
+
+} 
+}
+
  class Pedras {
     public int Numero {get; set;}
     public string Nome {get; set;}
@@ -75,8 +93,51 @@ static int contador = 1;
 
     public override string ToString()
     {
-        return $"{Numero}. {Nome} - {Valor} - {Tipo} - {Fornecedor} {Medida} - {Estoque} Estoque";
+        return $"{Numero}. {Nome} - {Valor} - {Tipo} - {Fornecedor} - {Medida} - {Estoque} Estoque";
 
     }
+
+}
+class Estoque{
+    static List<Pedras> pedras = new List<Pedras>();
+    static int contador = 1;
+
+    public void Novo(string nome, string valor, string tipo, string fornecedor, string medida) {
+        Pedras material = new Pedras {
+            Numero = contador, Nome = nome, Valor = valor, Tipo = tipo, Fornecedor = fornecedor, Medida = medida, Estoque = 0
+        };
+
+    pedras.Add(material);
+    contador++; Console.WriteLine("Produto adcionado");
+
+    }
+    public void ListarProdutos(){
+        if (pedras.Count == 0)
+
+        {Console.WriteLine(" sua lista está vazia.");
+        
+            
+        }else
+        {
+            foreach (var  pedra in pedras)
+            {
+                Console.WriteLine(pedra);
+            }
+        }
+    }
+     public void RemoverProdutos(int numero){
+        var produtoRemovido = pedras.FirstOrDefault(a=>a.Numero == numero);
+        if (produtoRemovido == null)
+            
+    
+        { Console.WriteLine("Esse produto não existe.");
+            
+        }
+else
+{
+    pedras.Remove( produtoRemovido );
+     Console.WriteLine("Seu produto foi removido com sucesso.");
+}
+     }
 
 }
--- obj/Debug/net8.0/controleestoque.AssemblyInfo.cs
+++ obj/Debug/net8.0/controleestoque.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("controleestoque")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9cfad3bcdda706f4f2735a1699fdc38a535bf01")]
 [assembly: System.Reflection.AssemblyProductAttribute("controleestoque")]
 [assembly: System.Reflection.AssemblyTitleAttribute("controleestoque")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
